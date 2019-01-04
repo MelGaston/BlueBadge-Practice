d5cda2048aabe6502b665fcf16d4b2b8bf2abe12
Melanie Gaston <melgaston@Melanies-MacBook-Air.local>
2019-01-04 15:15:26 -0500

    First Push

---
+++ 00-unary-operators.js
@@ -0,0 +1,17 @@
+// typeof
+
+let y = 7;
+let str = "Eleven Fifty Academy";
+let obj = {};
+let arr = []
+
+console.log(typeof y); // Number
+console.log(typeof str); // String
+console.log(typeof obj); // Object
+console.log(typeof arr); // Object
+
+// instanceof
+
+let x = [1,2,3,4,5];
+
+console.log(x instanceof Array); // True
---
+++ 01-mapping.js
@@ -0,0 +1,6 @@
+let nums = [1,2,3,4];
+let newNums = nums.map(el => {
+    return el;
+})
+
+console.log(newNums);
